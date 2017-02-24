556be2ed7b5bdc75842c788ec9cfcf2d6653780f
Ivan Granados <chivox.ivan@gmail.com>
2017-02-23 19:12:49 -0500

    force ssl on prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Inaturalist::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
+  config.force_ssl = true
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
